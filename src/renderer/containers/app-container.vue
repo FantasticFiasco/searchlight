5edaf2b0ac5b9fdde993a8c4082b270373f13c8c
FantasticFiasco <mattias.kindborg@gmail.com>
2017-08-31 19:38:22 +0200

    add app container

---
+++ src/renderer/containers/app-container.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="app">
+        <div class="app-body">
+            <main class="main">
+                <div class="container-fluid">
+                    <router-view></router-view>
+                </div>
+            </main>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component';
+
+@Component
+export default class AppContainer extends Vue {
+}
+</script>
--- src/renderer/router/index.ts
+++ src/renderer/router/index.ts
@@ -3,26 +3,34 @@ import Router from 'vue-router';
 import About from './../views/about.vue';
 import Devices from './../views/devices.vue';
 import Settings from './../views/settings.vue';
+import AppContainer from './../containers/app-container.vue';
 
 Vue.use(Router);
 
 export default new Router({
     routes: [
         {
-            name: 'devices',
-            path: '/devices',
-            alias: '/',
-            component: Devices,
-        },
-        {
-            name: 'settings',
-            path: '/settings',
-            component: Settings,
-        },
-        {
-            name: 'about',
-            path: '/about',
-            component: About,
-        },
+            name: 'home',
+            path: '/',
+            redirect: { name: 'devices' },
+            component: AppContainer,
+            children: [
+                {
+                    name: 'devices',
+                    path: 'devices',
+                    component: Devices,
+                },
+                {
+                    name: 'settings',
+                    path: 'settings',
+                    component: Settings,
+                },
+                {
+                    name: 'about',
+                    path: 'about',
+                    component: About,
+                },
+            ]
+        }
     ],
 });
