0e158ba154c5191a6bb2e0c0bb119e2f21b3abb1
FantasticFiasco <mattias.kindborg@gmail.com>
2017-09-10 09:24:15 +0200

    update footer

--- src/renderer/components/footer.vue
+++ src/renderer/components/footer.vue
@@ -1,9 +1,6 @@
 <template>
     <footer class="app-footer">
-        <a href="http://coreui.io">CoreUI</a> &copy; 2017 creativeLabs.
-        <span class="float-right">Powered by
-            <a href="http://coreui.io">CoreUI</a>
-        </span>
+        1 device / ? devices
     </footer>
 </template>
 
