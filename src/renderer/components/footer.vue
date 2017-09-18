4c5fe996611a3cbd31281471e2cf236f3b504716
FantasticFiasco <mattias.kindborg@gmail.com>
2017-09-18 20:14:08 +0200

    add store

--- src/renderer/components/footer.vue
+++ src/renderer/components/footer.vue
@@ -1,17 +1,20 @@
 <template>
     <footer class="app-footer">
-        1 device / ? devices
+        {{ count }} device / ? devices
     </footer>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import 'vuex';
 import Component from 'vue-class-component';
 
 @Component({
     name: 'footer',
 })
 export default class Footer extends Vue {
+    public get count() {
+        return this.$store.state.devices.length;
+    }
 }
 </script>
-
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,13 +1,16 @@
 import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
+
 import App from './app.vue';
 import Router from './router';
+import Store from './store';
 
 Vue.use(BootstrapVue);
 
 // tslint:disable:no-unused-expression
 new Vue({
     el: '#app',
+    store: Store,
     router: Router,
     render: (h) => h(App),
 });
