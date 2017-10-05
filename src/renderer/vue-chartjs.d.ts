45c15edcb7d2bd9be07e7cb29d1f4eda39fbc4b2
FantasticFiasco <mattias.kindborg@gmail.com>
2017-10-05 22:13:49 +0200

    wip

--- src/renderer/vue-chartjs.d.ts
+++ src/renderer/vue-chartjs.d.ts
@@ -2,8 +2,8 @@ declare module 'vue-chartjs' {
     import Vue from 'vue';
 
     class Bar extends Vue {
-        public renderChart(data: any, options: any): void;
         public _chart: Chart;
+        public renderChart(data: any, options: any): void;
     }
 
     class Chart {
