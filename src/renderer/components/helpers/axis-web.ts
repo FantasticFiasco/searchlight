95a4e3b02e3666e826048652e3f45abf2c12c2b4
Mattias Kindborg <mattias.kindborg@gmail.com>
2017-10-09 23:38:52 +0200

    wip

--- src/renderer/components/helpers/axis-web.ts
+++ src/renderer/components/helpers/axis-web.ts
@@ -3,8 +3,7 @@ export function productPageUrl(modelName: string | undefined): string {
         return 'https://www.axis.com/products-and-solutions';
     }
 
-    modelName = modelName.replace(' ', '-').toLowerCase();
-    return `https://www.axis.com/products/${modelName}`;
+    return `https://www.axis.com/products/${adjustForModelNameExceptions(modelName)}`;
 }
 
 export function iconUrl(modelNumber: string | undefined): string {
@@ -14,4 +13,18 @@ export function iconUrl(modelNumber: string | undefined): string {
 
     modelNumber = modelNumber.toLowerCase();
     return `https://www.axis.com/images/scaled/300/sites/default/files/${modelNumber}.png`;
+}
+
+function adjustForModelNameExceptions(modelName: string): string {
+    if (modelName === 'AXIS M3005') {
+        modelName = 'AXIS M3005-V';
+    } else if (modelName === 'AXIS M3007') {
+        modelName = 'AXIS M3007-P';
+    } else if (modelName === 'AXIS M3025') {
+        modelName = 'AXIS M3025-VE';
+    } else if (modelName === 'AXIS P3367') {
+        modelName = 'AXIS P3367-V';
+    }
+
+    return modelName.replace(' ', '-').toLowerCase();
 }
