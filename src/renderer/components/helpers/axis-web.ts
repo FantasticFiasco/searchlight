560a6fd36a4dffdeb023b01671c4f4d11e4ad02b
FantasticFiasco <mattias.kindborg@gmail.com>
2017-10-10 07:56:43 +0200

    wip

--- src/renderer/components/helpers/axis-web.ts
+++ src/renderer/components/helpers/axis-web.ts
@@ -1,30 +1,40 @@
+/**
+ * Returns the product page URL on www.axis.com.
+ * @param modelName the model name
+ */
 export function productPageUrl(modelName: string | undefined): string {
     if (modelName === undefined) {
         return 'https://www.axis.com/products-and-solutions';
     }
 
-    return `https://www.axis.com/products/${adjustForModelNameExceptions(modelName)}`;
+    modelName = (modelNameExceptions[modelName] || modelName).replace(' ', '-').toLowerCase();
+    return `https://www.axis.com/products/${modelName}`;
 }
 
+/**
+ * Returns the icon URL on www.axis.com.
+ * @param modelNumber the model number
+ */
 export function iconUrl(modelNumber: string | undefined): string {
     if (modelNumber === undefined) {
         return '';
     }
 
-    modelNumber = modelNumber.toLowerCase();
+    modelNumber = (modelNumberExceptions[modelNumber] || modelNumber).toLowerCase();
     return `https://www.axis.com/images/scaled/300/sites/default/files/${modelNumber}.png`;
 }
 
-function adjustForModelNameExceptions(modelName: string): string {
-    if (modelName === 'AXIS M3005') {
-        modelName = 'AXIS M3005-V';
-    } else if (modelName === 'AXIS M3007') {
-        modelName = 'AXIS M3007-P';
-    } else if (modelName === 'AXIS M3025') {
-        modelName = 'AXIS M3025-VE';
-    } else if (modelName === 'AXIS P3367') {
-        modelName = 'AXIS P3367-V';
-    }
+const modelNameExceptions: { [key: string]: string } = {
+    'AXIS M3005': 'AXIS M3005-V',
+    'AXIS M3007': 'AXIS M3007-P',
+    'AXIS M3025': 'AXIS M3025-VE',
+    'AXIS P3367': 'AXIS P3367-V',
+};
 
-    return modelName.replace(' ', '-').toLowerCase();
-}
+const modelNumberExceptions: { [key: string]: string } = {
+    M3005: 'M3005-V',
+    M3007: 'M3007-P',
+    M3025: 'M3025-VE',
+    P1346: 'P1346_0',
+    P3367: 'P3367-V',
+};
