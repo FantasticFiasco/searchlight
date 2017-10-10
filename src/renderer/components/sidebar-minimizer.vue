9dae9cc429bdaa53576eeac5246f7556aa2887f6
FantasticFiasco <mattias.kindborg@gmail.com>
2017-10-10 20:03:02 +0200

    add sidebar minimized

---
+++ src/renderer/components/sidebar-minimizer.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="sidebar-minimizer" type="button" @click="sidebarMinimize" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component({ name: 'sidebar-minimizer' })
+export default class SidebarMinimizer extends Vue {
+    public sidebarMinimize() {
+        document.body.classList.toggle('sidebar-minimized')
+    }
+}
+</script>
