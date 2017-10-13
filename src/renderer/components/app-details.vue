eae0139be8198d16bcb43769abab527e0323c907
FantasticFiasco <mattias.kindborg@gmail.com>
2017-10-13 08:10:04 +0200

    wip

---
+++ src/renderer/components/app-details.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <p>Developed with
+            <i class="icon-heart"></i> by the Open Source community</p>
+        <p>Application: {{ appVersion }}</p>
+        <p>Electron: {{ electronVersion }}</p>
+        <p>Node.js: {{ nodeVersion }}</p>
+        <p>Chromium: {{ chromeVersion }}</p>
+        <p v-for="i in 100">{{ i }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { remote } from 'electron';
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component({ name: 'app details' })
+export default class AppDetails extends Vue {
+    get appVersion() {
+        return remote.app.getVersion();
+    }
+
+    get electronVersion() {
+        return process.versions.electron;
+    }
+
+    get nodeVersion() {
+        return process.versions.node;
+    }
+
+    get chromeVersion() {
+        return process.versions.chrome;
+    }
+}
+</script>
--- src/renderer/views/about.vue
+++ src/renderer/views/about.vue
@@ -1,44 +1,26 @@
 <template>
     <div class="animated fadeIn">
-        <h1>About</h1>
-        <p>Developed with
-            <i class="icon-heart"></i> by the Open Source community</p>
-        <p>Application: {{ appVersion }}</p>
-        <p>Electron: {{ electronVersion }}</p>
-        <p>Node.js: {{ nodeVersion }}</p>
-        <p>Chromium: {{ chromeVersion }}</p>
-        <p v-for="i in 100">{{ i }}</p>
+        <AppDetails />
     </div>
 </template>
 
 <script lang="ts">
-import { remote } from 'electron';
 import Vue from 'vue';
 import { Component, Inject } from 'vue-property-decorator';
 
+import AppDetails from '../components/app-details.vue';
 import { ANALYTICS_SERVICE } from '../dependency-injection';
 import { AnalyticsService, PageView } from '../services';
 
-@Component({ name: 'about' })
+@Component({
+    name: 'about',
+    components: {
+        AppDetails,
+    }
+})
 export default class About extends Vue {
     @Inject(ANALYTICS_SERVICE)
     private readonly analyticsService: AnalyticsService;
-
-    get appVersion() {
-        return remote.app.getVersion();
-    }
-
-    get electronVersion() {
-        return process.versions.electron;
-    }
-
-    get nodeVersion() {
-        return process.versions.node;
-    }
-
-    get chromeVersion() {
-        return process.versions.chrome;
-    }
 
     public mounted() {
         this.analyticsService.reportPageView(new PageView('/about'));
