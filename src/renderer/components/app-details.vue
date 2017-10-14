85220da4234d69e1f654a1eb002c5dcf151033ee
Mattias Kindborg <mattias.kindborg@gmail.com>
2017-10-14 14:07:49 +0200

    wip

--- src/renderer/components/app-details.vue
+++ src/renderer/components/app-details.vue
@@ -1,41 +1,53 @@
 <template>
     <div>
-        <b-card title="Searchlight">
-            <p class="card-text">
-                Version: {{ appVersion }}
-            </p>
-            <p class="card-text">
-                Report an issue
-            </p>
+        <b-card class="app-details-card">
+            <h4 slot="header">Searchlight</h4>
+            <div class="ml-2">
+                <p class="card-text">
+                    <i class="fa fa-check-circle text-success" /> Version {{ appVersion }}
+                </p>
+                <p class="card-text">
+                    <a @click="openIssueWebPage" href="">Report an issue</a>
+                </p>
+                <p class="card-text">Searchlight is made possible by the
+                    <a @click="openElectronWebPage" href="#">Electron</a> open source project and the APIs provided by
+                    <a @click="openAxisCommunicationsWebPage" href="#">Axis Communications</a>.
+                </p>
+            </div>
         </b-card>
-        <b-card sub-title="Dependencies">
-            <b-list-group flush>
-                <b-list-group-item>
-                    <h4>Searchlight</h4>
-                </b-list-group-item>
-                <b-list-group-item>
-                    <h6>Dependencies</h6>
-
-                </b-list-group-item>
-                <b-list-group-item>Electron: {{ electronVersion }}</b-list-group-item>
-                <b-list-group-item>Node.js: {{ nodeVersion }}</b-list-group-item>
-                <b-list-group-item>Chromium: {{ chromeVersion }}</b-list-group-item>
-                <b-list-group-item></b-list-group-item>
-            </b-list-group>
+        <b-card class="app-details-card">
+            <h6 slot="header">Dependencies</h6>
+            <div class="ml-2">
+                <p class="card-text">
+                    Searchlight has dependencies to the following components:
+                </p>
+                <ul>
+                    <li class="card-text">
+                        Electron version {{ electronVersion }}
+                    </li>
+                    <li class="card-text">
+                        Node.js version {{ nodeVersion }}
+                    </li>
+                    <li class="card-text">
+                        Chromium version {{ chromeVersion }}
+                    </li>
+                </ul>
+            </div>
         </b-card>
-        <b-card no-body>
-            <b-card-text>Google Chrome</b-card-text>
-            <b-card-text>Copyright 2017 Google Inc. All rights reserved.</b-card-text>
-
-            <!-- <!-- <h1>About</h1>
-                                                                                                <p>Developed with
-                                                                                                    <i class="icon-heart"></i> by the Open Source community</p>
-                                                                                        <p>Application: {{  }}</p>--!> -->
+        <b-card class="app-details-card">
+            <h6 slot="header">Legal</h6>
+            <div class="ml-2">
+                <p class="card-text">
+                    <a @click="openLicenseWebPage" href="#">License</a>
+                </p>
+                <p class="card-text">Copyright 2017 Mattias Kindborg</p>
+            </div>
         </b-card>
     </div>
 </template>
 
 <script lang="ts">
+import { shell } from 'electron';
 import { remote } from 'electron';
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
@@ -56,6 +68,30 @@ export default class AppDetails extends Vue {
 
     get chromeVersion() {
         return process.versions.chrome;
+    }
+
+    public openIssueWebPage(e: Event) {
+        e.preventDefault();
+
+        shell.openExternal('https://github.com/FantasticFiasco/searchlight/issues/new');
+    }
+
+    public openElectronWebPage(e: Event) {
+        e.preventDefault();
+
+        shell.openExternal('https://electron.atom.io/');
+    }
+
+    public openAxisCommunicationsWebPage(e: Event) {
+        e.preventDefault();
+
+        shell.openExternal('https://www.axis.com/');
+    }
+
+    public openLicenseWebPage(e: Event) {
+        e.preventDefault();
+
+        shell.openExternal('https://github.com/FantasticFiasco/searchlight/blob/master/LICENSE');
     }
 }
 </script>
