1e1f9225da337be3857af845a10f739880fe928d
FantasticFiasco <mattias.kindborg@gmail.com>
2017-12-23 10:11:54 +0100

    wip

--- src/main/index.ts
+++ src/main/index.ts
@@ -99,7 +99,7 @@ app.on('ready', createMainWindow);
 app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the dock icon
     // is clicked and there are no other windows open.
-    if (mainWindow === null) {
+    if (mainWindow === undefined) {
         createMainWindow();
     }
 });
