cdd7697fec44921d089e9f72f196b73763997be9
FantasticFiasco <mattias.kindborg@gmail.com>
2018-01-01 07:40:38 +0100

    wip

--- src/renderer/app.vue
+++ src/renderer/app.vue
@@ -14,8 +14,6 @@ import {
     ApplicationUpdatesService,
     AXIS_WEB_SERVICE,
     AxisWebService,
-    DEVICE_ICON_SERVICE,
-    DeviceIconService,
     DISCOVERY_SERVICE,
     DiscoveryService,
     HEARTBEAT_SERVICE,
@@ -26,7 +24,6 @@ import * as vueHandlers from './vue-handlers';
 const analyticsService = new AnalyticsService();
 const applicationUpdatesService = new ApplicationUpdatesService();
 const axisWebService = new AxisWebService();
-const deviceIconService = new DeviceIconService();
 const discoveryService = new DiscoveryService();
 const heartbeatService = new HeartbeatService(discoveryService);
 
@@ -39,7 +36,6 @@ vueHandlers.reportExceptions(analyticsService);
         [ANALYTICS_SERVICE]: analyticsService,
         [APPLICATION_UPDATES_SERVICE]: applicationUpdatesService,
         [AXIS_WEB_SERVICE]: axisWebService,
-        [DEVICE_ICON_SERVICE]: deviceIconService,
         [DISCOVERY_SERVICE]: discoveryService,
         [HEARTBEAT_SERVICE]: heartbeatService,
     },
--- src/renderer/services/index.ts
+++ src/renderer/services/index.ts
@@ -3,7 +3,6 @@ export * from 'common/application-updates';
 export * from 'common/discovery';
 export * from './analytics-service';
 export * from './application-updates-service';
-export * from './device-icon-service';
 export * from './discovery-service';
 export * from './heartbeat-service';
 export * from './axis-web-service';
